e2b9be6310554f6bc6136c8b05afe8dadd27bfb2
Maxim Grassi <grassimacsim@yandex.ru>
2023-04-14 23:33:07 +0300

    feat: добавить модели и схемы

--- app.js
+++ app.js
@@ -1,17 +1,70 @@
+// подключение всех библиотек
 const express = require('express');
 const mongoose = require('mongoose');
 
+// запуск сервера с дефолтным портом 3000
 const app = express();
-
 const { PORT = 3000 } = process.env;
 
+// подключение к базе данных mestodb
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb').then(() => {
   console.log('Connecting mongo');
 }).catch((err) => {
   console.log(err);
 });
 
+// схема пользователя
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+// схема карточки
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  likes: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: [],
+  },
+  createdAte: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// создание модели пользователя и карточки
+module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('card', cardSchema);
+
+// приложение слушает соединения на заданном порте
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
