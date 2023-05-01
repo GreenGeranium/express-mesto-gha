843a1ce92d17edc9477dbf44bf1315e87f9921ab
Maxim Grassi <grassimacsim@yandex.ru>
2023-05-01 13:18:50 +0300

    fix: добавить валидацию ссылки в joi 1

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().regex(/https?:\/\/(w{3}.)?([0-9A-Za-z-]{1,}).([A-Za-z]){1,}?([0-9A-Za-z-._~:?#@!$&'()*+,;=\/\[\]]{1,})#?/m),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }).unknown(true),
