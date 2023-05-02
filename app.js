d2cdae51597755e4729ff8b03119edf6b1a8c31d
Maxim Grassi <grassimacsim@yandex.ru>
2023-05-02 11:09:51 +0300

    fix: app

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const users = require('./routes/users');
 const cards = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
+const NotFoundError = require('./errors/not-found-err');
+const AlreadyExistsErr = require('./errors/already-exists');
 
 // запуск сервера с дефолтным портом 3000
 const app = express();
@@ -41,9 +43,7 @@ app.use(auth);
 app.use('/users', users);
 app.use('/cards', cards);
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'Извините, такой страницы не существует!' });
-});
+app.use((req, res) => NotFoundError('Извините, такой страницы не существует!'));
 
 // глобальный обработчик ошибок
 app.use(errors());
@@ -51,15 +51,8 @@ app.use((err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
   console.log(err); // Log the entire request object
-  if (err.name === 'ValidationError') {
-    const errorsList = Object.values(err.errors).map((error) => error.message);
-    return res.status(400).send({ message: 'Данные не валидны', errorsList });
-  } if (err.name === 'CastError') {
-    return res
-      .status(400)
-      .send({ message: 'Некорректный идентификатор' });
-  } if (err.code === 11000) {
-    return res.status(409).send({ message: 'Данный профиль уже существует' });
+  if (err.code === 11000) {
+    return AlreadyExistsErr('Данный профиль уже существует');
   } return res.status(statusCode).send({
     message: statusCode === 500
       ? 'На сервере произошла ошибка'
